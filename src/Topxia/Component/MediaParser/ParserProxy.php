dcc2b8bbd9d5a318b80ee7800146e8e0dd69c906
WRLin <guangxiao.wang@gmail.com>
2014-09-18 18:16:24 +0800

    添加针对CC视频得支持

--- src/Topxia/Component/MediaParser/ParserProxy.php
+++ src/Topxia/Component/MediaParser/ParserProxy.php
@@ -7,6 +7,9 @@ class ParserProxy
     public function parseItem($url)
     {
         $parsers = array('YoukuVideo', 'QQVideo', 'NeteaseOpenCourse', 'TudouVideo');
+        //新增用于扩展视频类型
+        $parsersExtension = array('Bokecc');
+        $parsers = array_merge($parsers, $parsersExtension);
 
         foreach ($parsers as $parserName) {
             $class = __NAMESPACE__ . "\\ItemParser\\{$parserName}ItemParser";
